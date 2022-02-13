98d77927e77bf4ecbef44b1ed66090d9db486933
Matiata <matizmurat@gmail.com>
2022-02-13 16:59:56 -0300

    Name Filter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import { Stack } from '@mui/material';
-import { useState } from 'react';
-import { TextField } from '@mui/material';
 import LaunchesTable from './components/LaunchesTable';
+import './App.css'
 
 function App() {
-  const [filter, setFilter] = useState('');
   return (
     <div>
-      <h2> SpaceX Launches </h2>
-      <TextField 
-        id='filter'
-        label='Name Filter'
-        onChange={(event) => setFilter(event.target.value)}
-      />
-      {/*<Stack direction={'row'}>
-      </Stack> */}
+      <h2 className='title'> SpaceX Satellite Launches </h2>
       <LaunchesTable />
     </div>
   );
--- src/components/LaunchesTable.js
+++ src/components/LaunchesTable.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Checkbox, Table, TableBody, TableCell, TableFooter,
-  TableHead, TablePagination, TableRow } from '@mui/material';
+import { Checkbox, Stack, Table, TableBody, TableCell, TableFooter,
+  TableHead, TablePagination, TableRow, TextField } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
-import RefreshIcon from '@mui/icons-material/Refresh'
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 async function fetchData(){
   const endpoint = 'https://api.spacexdata.com/v5/launches/';
@@ -26,26 +28,31 @@ async function fetchData(){
 
 function LaunchesTable(){
   const [rows, setRows] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [successFilter, setSuccessFilter] = useState(false);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [dataLength, setDataLength] = useState(0);
   const [page, setPage] = useState(0);
-  const [refresh, setRefresh] = useState(false);
+  const [refresh, setRefresh] = useState(true);
 
   function showLaunches(rows){
-    let res = []
-    if(rows){
-      if(rows.length > 0){
-        res = rows
-        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-        .map((row) =>(
-          <TableRow key={row.id}>
-            <TableCell> {row.name} </TableCell>
-            <TableCell> {row.date_utc} </TableCell>
-            <TableCell>
-              {row.success ? <Checkbox checked/> : <Checkbox checked={false}/>}
-            </TableCell>
-          </TableRow>
-        ))
+    let res = [];
+    if(rows && rows.length > 0){
+      res = rows;
+      if(!(filter === '')){
+        res = rows.filter(row => row.name.includes(filter));
       }
+      res = res
+      .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+      .map((row) =>(
+        <TableRow key={row.id}>
+          <TableCell> {row.name} </TableCell>
+          <TableCell> {row.date_utc} </TableCell>
+          <TableCell>
+            {row.success ? <CheckIcon /> : <CloseIcon />}
+          </TableCell>
+        </TableRow>
+      ))
     }
     return res;
   }
@@ -79,6 +86,7 @@ function LaunchesTable(){
     }
 
     updateLaunches();
+    setDataLength(rows.length);
 
     return () => {
       isMounted = false;
@@ -86,22 +94,48 @@ function LaunchesTable(){
 
   },[refresh]);
 
+   useEffect(() => {
+    let dataSize = 0;
+    rows.filter(row => {
+      if(row.name.includes(filter)){
+        dataSize += 1;
+      }
+    })
+
+    setDataLength(dataSize);
+
+  },[filter]) 
+
   return(
     <div>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>
-              Name
+              <Stack direction={"row"} alignItems="center" spacing={2}>
+                <div>
+                  Name
+                </div>
+                <TextField
+                  id='filter'
+                  label='Name Filter'
+                  onChange={(event) => setFilter(event.target.value)}
+                />
+              </Stack>
             </TableCell>
             <TableCell>
               Date
             </TableCell>
             <TableCell>
-              Success
+              <Stack direction={"row"} alignItems="center" spacing={0}>
+                <div>
+                  Success
+                </div>
+                <Checkbox onClick={() => setSuccessFilter(true)}/>
+              </Stack>
             </TableCell>
             <TableCell>
-              <IconButton onClick={() => {setRefresh(true)}}>
+              <IconButton onClick={() => setRefresh(true)}>
                 <RefreshIcon />
               </IconButton>
             </TableCell>
@@ -115,7 +149,7 @@ function LaunchesTable(){
             <TablePagination 
               rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
               component="div"
-              count={rows.length}
+              count={dataLength}
               rowsPerPage={rowsPerPage}
               page={page}
               onPageChange={handleChangePage}
